4b095e90dc983f45c1c4134f3d5f0b89bd034458
okh555 <okh8522@gmail.com>
2020-09-02 16:12:29 +0900

    Init

---
+++ Assets/Shoot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject arrowPrefab;
+    public Transform arrowSpawn;
+    public float shootForce = 10f;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+       // shoot();
+    }
+    // Update is called once per frame
+    public void shoot()
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GameObject arrow = Instantiate(arrowPrefab, arrowSpawn.position, Quaternion.identity);
+        Transform trans = arrow.GetComponent<Transform>();
+        Rigidbody rb = arrow.GetComponent<Rigidbody>();
+        rb.velocity = arrowSpawn.forward * shootForce;
+        animator.SetBool("Shoot", true);
+    }
+    public void discard()
+    {
+        animator.SetBool("Shoot", false);
+    }
+}
